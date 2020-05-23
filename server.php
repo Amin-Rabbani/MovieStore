d6bb543b52505e02aa00ef555641380754070683
Amin <a.rabani1379@gmail.com>
2020-05-23 22:52:33 +0430

    register login logout addFilm

---
+++ server.php
@@ -0,0 +1,100 @@
+<?php
+    session_start();
+    $username = "";
+    $email = "";
+    $role = "";
+
+    $db = mysqli_connect("localhost", "phpmyadmin", "root", "MovieStore") or die("Could not connect to the DataBase !");
+
+    $errors = array();
+    
+    if(isset($_POST['register_user'])){
+        //Register User
+        
+        $username = mysqli_real_escape_string($db, $_POST['username']);    
+        $email = mysqli_real_escape_string($db, $_POST['email']);    
+        $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+        $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);        
+        $role = mysqli_real_escape_string($db, $_POST['role']);    
+
+        //Validation
+
+        if(empty($username)) array_push($errors, "UserName is Required");
+        if(empty($email)) array_push($errors, "Email is Required");
+        if(empty($password_1)) array_push($errors, "Password is Required");
+        if($password_1 != $password_2) array_push($errors, "Passwords don't match");
+
+        //Check if username already exist
+
+        $user_check_query = "SELECT * FROM Members WHERE username='$username' OR email='$email'";
+        $result = mysqli_query($db, $user_check_query);
+        $user = mysqli_fetch_assoc($result);
+
+        if($user){
+
+            if($user['username'] == $username) array_push($errors, "UserName already exist");
+            if($user['email'] == $email) array_push($errors, "Email already exist");
+        }
+        if(count($errors) == 0){
+
+            $password = md5($password_1);
+            $query = "INSERT INTO Members (username, email, password, role) VALUES 
+                        ('$username', '$email', '$password', '$role')";
+            mysqli_query($db, $query);
+            $_SESSION['username'] = $username;
+            $_SESSION['email'] = $email;
+            $_SESSION['role'] = $role;
+            $_SESSION['msg'] = "You registered Successfully !";
+            header('Location: index.php');
+        }
+    }
+
+    //Login User
+
+    if(isset($_POST['login_user'])){
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+        
+        if(empty($username)) array_push($errors, "UserName is Required");
+        if(empty($password)) array_push($errors, "Password is Required");
+        
+        $password = md5($password);
+        $query = "SELECT * FROM Members WHERE username='$username' AND password='$password'";
+        $result = mysqli_query($db, $query);
+        $data = mysqli_fetch_assoc($result);
+
+        if(mysqli_num_rows($result)){
+            $_SESSION['username'] = $username;
+            $_SESSION['role'] = $data['role'];
+            $_SESSION['msg'] = "You logged in Successfully !";
+            header('Location: index.php');
+        }else{
+            array_push($errors, "Wrong UserName or Password!");
+        }
+    }
+    
+    //LogOut User
+
+    if(isset($_GET['logout'])){
+        session_destroy();
+        header('Location: login.php');
+    }
+
+    //Add Film
+
+    if(isset($_POST['addFilm'])){
+        $name = mysqli_real_escape_string($db, $_POST['name']);
+        $director = mysqli_real_escape_string($db, $_POST['director']);
+        $genre = mysqli_real_escape_string($db, $_POST['genre']);
+        $price = mysqli_real_escape_string($db, $_POST['price']);
+        $summary = mysqli_real_escape_string($db, $_POST['summary']);
+        $publisher = $_SESSION['username'];
+
+        $query = "INSERT INTO Movies (name, director, genre, publisher, price, summary) VALUES 
+                    ('$name', '$director', '$genre', '$publisher', '$price', '$summary')";
+        mysqli_query($db, $query);
+        $_SESSION['msg'] = "Film added Successfully !";
+        header('Location: index.php');
+    }
+
+?>
