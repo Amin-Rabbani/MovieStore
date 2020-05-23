c882a3fbeb5580a72b312bf4ed0aa37a1d970f10
Amin <a.rabani1379@gmail.com>
2020-05-23 17:10:01 +0430

    Login page completed

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php 
+    require('server.php'); 
+    include('navbar.php');
+?>
+<html>
+<head>
+    <title> Login </title>
+</head>
+<body style="text-align: center;">
+    <br><br><br><br><br><br>
+    <form method="POST" action="login.php">
+        <p><b>Login</b></p><br>
+        <label for="username"> UserName :</label><br>
+        <input type="text" name="username" id="username" required><br><br>
+        <label for="password"> Password</label><br>
+        <input type="password" name="password" id="password" required><br><br>
+        <button type="submit" name="login_user" id="login_user"> Login </button>
+    </form>
+    <br><br>
+    <a href="register.php"> Don't Have an Account ?</a><br>
+</body>
+</html>
